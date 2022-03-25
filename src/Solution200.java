8c75f4d640ccdaff2f3d7a81ae88226a249092cd
dong <hedong2001@outlook.com>
2022-03-26 04:36:02 +0800

    200.岛屿数量

---
+++ src/Solution200.java
@@ -0,0 +1,24 @@
+public class Solution200 {
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++){
+                if (grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    ans++;
+                }
+            }
+        return ans;
+    }
+
+    void dfs(char[][] grid, int x, int y){
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == '0'){
+            return;
+        }
+        grid[x][y] = '0';
+        dfs(grid, x-1, y);
+        dfs(grid, x+1, y);
+        dfs(grid, x, y-1);
+        dfs(grid, x, y+1);
+    }
+}
