bb170d4be240afdfa5fe9b076378d9da28b21390
dong <hedong2001@outlook.com>
2022-03-27 01:43:14 +0800

    437.路径总和 III

--- src/Solution437.java
+++ src/Solution437.java
@@ -1,5 +1,26 @@
-public class Solution437 {
-    public int pathSum(TreeNode root, int targetSum) {
+import java.util.HashMap;
 
+public class Solution437 {
+
+    int ans;
+    int target;
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+        ans = 0;
+        target = targetSum;
+        map.put(0,1);
+        dfs(root, root.val);
+        return ans;
+    }
+
+    void dfs(TreeNode node, int sum){
+        if (node == null)   return;
+        if (map.containsKey(sum - target)) ans += map.get(sum - target);
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+        if (node.left != null ) dfs(node.left, sum + node.left.val);
+        if (node.right != null ) dfs(node.right, sum + node.right.val);
+        map.put(sum, map.getOrDefault(sum, 0) - 1);
     }
 }
