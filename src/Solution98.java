fed163fa59cbd97720fd0627fd22266d17f42f71
Dong He <hedong2001@outlook.com>
2022-04-29 02:04:28 +0800

    98.验证二叉搜索树

---
+++ src/Solution98.java
@@ -0,0 +1,11 @@
+public class Solution98 {
+    long pre = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        if (!isValidBST(root.left)) return false;
+        if (root.val <= pre)  return false;
+        pre = root.val;
+        return isValidBST(root.right);
+    }
+}
+
